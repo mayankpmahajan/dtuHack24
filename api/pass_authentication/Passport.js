9740d1cf5cfe9ebae79dab32795fe70184bd4ecb
Realist2011 <satvikbajaj20@gmail.com>
2024-07-08 00:55:21 +0530

    Changes in google

--- api/pass_authentication/Passport.js
+++ api/pass_authentication/Passport.js
@@ -7,8 +7,7 @@ passport.use(
     
     new GoogleStrategy(
   {
-    clientID:'105310426288-j3mdbl3r3944a4uiiu1p5cm667f0ee0s.apps.googleusercontent.com',
-    clientSecret:' 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',
+    
     callbackURL: "http://localhost:8800/api/auth/auth/google/callback",
   },
   
