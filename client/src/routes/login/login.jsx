2427a00accf515c4827ef461e2aeadc492e340a0
rafool05 <rj05jain@gmail.com>
2024-07-09 02:06:10 +0530

    second commit

--- client/src/routes/login/login.jsx
+++ client/src/routes/login/login.jsx
@@ -57,7 +57,7 @@ function Login() {
           />
           <button disabled={isLoading}>Login</button>
           {error && <span>{error}</span>}
-          <Link to="/register">{"Don't"} you have an account?</Link>
+          <Link to="/register">Don't have an account?</Link>
         </form>
       </div>
       {/* <div className="imgContainer">
